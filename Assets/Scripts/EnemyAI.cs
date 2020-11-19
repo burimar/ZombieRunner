36620dc275fefb31f0d0fed621478e0321c9af53
Marco Buri <burima@protonmail.ch>
2020-11-19 19:00:46 +0100

    initial commit

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 10;
+
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (distanceToTarget <= chaseRange)
+        {
+            EngageTarget();
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
+
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            Debug.Log("Attacking!!!");
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+
+}
