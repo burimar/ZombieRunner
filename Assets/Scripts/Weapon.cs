8651c23884f1ad0c5d1fbac9ea938b2d1ad45b0d
Marco Buri <burima@protonmail.ch>
2020-11-19 19:16:40 +0100

    adds raycast to weapon

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera firstPersonCam;
+    [SerializeField] float range = 100f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(firstPersonCam.transform.position, firstPersonCam.transform.forward, out hit, range);
+        Debug.Log("I hit: " + hit.transform.name);
+    }
+}
