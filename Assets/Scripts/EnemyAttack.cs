d3bba09f5139aa7d5176ca51c5516d95c65f8540
Marco Buri <burima@protonmail.ch>
2020-11-22 17:18:29 +0100

    adds enemy attack animation event

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] Transform target = default;
+    [SerializeField] float damage = 40f;
+
+    void Start()
+    {
+        
+    }
+
+    public void AttackHitEvent()
+    {
+        if (target == null) return;
+
+        Debug.Log("Hit you noob");
+
+    }
+    
+}
