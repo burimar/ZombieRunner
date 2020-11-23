294b2359ddf0e657efff46942ba74173f85976c4
Marco Buri <burima@protonmail.ch>
2020-11-23 22:11:18 +0100

    adds zoom to weapon

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+
+    [SerializeField] float zoomIn = 25;
+
+    private Camera camera;
+    private float zoomOriginal = 0f;
+
+
+    void Start()
+    {
+        camera = GetComponentInChildren<Camera>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            ToggleZoom();
+        }
+    }
+
+    private void ToggleZoom()
+    {
+        if (zoomOriginal > 0f)
+        {
+            camera.fieldOfView = zoomOriginal;
+            zoomOriginal = 0f;
+        } else
+        {
+            zoomOriginal = camera.fieldOfView;
+            camera.fieldOfView = zoomIn;
+        }
+    }
+
+}
