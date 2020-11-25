7996288752a66fa79614d19012fb73442182368d
Marco Buri <burima@protonmail.ch>
2020-11-25 20:39:41 +0100

    adds pistole and wepon switcher

---
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class WeaponSwitcher : MonoBehaviour
+{
+
+    [SerializeField] int currentWeapon = 0;
+
+    void Start()
+    {
+        ActivateWeapon();
+    }
+
+    private void ActivateWeapon()
+    {
+        int i = 0;
+        foreach (Transform weapon in transform)
+        {
+            weapon.gameObject.SetActive(i == currentWeapon);
+            i++;
+        }
+    }
+}
