a7159de3dd9cb1ea34ba4831d4091627dfdfe15c
Marco Buri <burima@protonmail.ch>
2020-11-25 21:16:55 +0100

    implements weapon switching with number keys and mouse scroll wheel

--- Assets/Scripts/WeaponSwitcher.cs
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -1,16 +1,47 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class WeaponSwitcher : MonoBehaviour
 {
-
     [SerializeField] int currentWeapon = 0;
+
+    private int nbrOfWeapons;
 
     void Start()
     {
-        ActivateWeapon();
+        nbrOfWeapons = transform.childCount;
+        SetActiveWeapon();
     }
 
-    private void ActivateWeapon()
+    void Update()
+    {
+        int previousWeapon = currentWeapon;
+        
+        ProcessKeyInput();
+        ProcessMouseWheel();
+
+        if (currentWeapon != previousWeapon) SetActiveWeapon();
+    }
+
+    private void ProcessMouseWheel()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            currentWeapon = ((currentWeapon - 1) + nbrOfWeapons) % nbrOfWeapons;
+        } else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            currentWeapon = (currentWeapon + 1) % nbrOfWeapons;
+        }
+    }
+
+    private void ProcessKeyInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) currentWeapon = 0;
+        if (Input.GetKeyDown(KeyCode.Alpha2)) currentWeapon = 1;
+        if (Input.GetKeyDown(KeyCode.Alpha3)) currentWeapon = 2;
+    }
+
+    private void SetActiveWeapon()
     {
         int i = 0;
         foreach (Transform weapon in transform)
