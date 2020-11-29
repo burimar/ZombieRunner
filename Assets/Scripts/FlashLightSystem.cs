e84be7ee4fbb7b85015a3cd6042b7666f4f1e3f9
Marco Buri <burima@protonmail.ch>
2020-11-29 16:18:46 +0100

    adds simple flash light that is fading out

---
+++ Assets/Scripts/FlashLightSystem.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+    [SerializeField] float intensityDecay = 0.1f;
+
+    Light flashLight = default;
+
+    void Start()
+    {
+        flashLight = GetComponent<Light>();
+    }
+
+    
+    void Update()
+    {
+        DecreaseLightIntensity();
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        if (flashLight.intensity > 0)
+        {
+            flashLight.intensity -= intensityDecay * Time.deltaTime;
+        }
+    }
+
+}
