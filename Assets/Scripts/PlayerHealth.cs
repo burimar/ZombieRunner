249d357832d604457fb5485c66cea83400cf8a53
Marco Buri <burima@protonmail.ch>
2020-12-01 21:16:16 +0100

    adds damage taken canvas with blood splatter

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,10 +1,30 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] float health = 100f;
+    [SerializeField] Canvas damageTakenCanvas;
+
+    void Start()
+    {
+        damageTakenCanvas.enabled = false;
+    }
 
     public void TakeDamage(float damage)
+    {
+        StartCoroutine(ShowDamageReceivedCanvas());
+        DecreaseHealth(damage);
+    }
+
+    private IEnumerator ShowDamageReceivedCanvas()
+    {
+        damageTakenCanvas.enabled = true;
+        yield return new WaitForSeconds(0.3f);
+        damageTakenCanvas.enabled = false;
+    }
+
+    private void DecreaseHealth(float damage)
     {
         health -= damage;
         if (health <= 0)
